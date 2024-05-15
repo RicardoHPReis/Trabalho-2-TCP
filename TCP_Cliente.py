332f7f259c09e9de03cd15ed681b9556a76bb0f6
Ricardo Reis <ricardohenriquepreis@gmail.com>
2024-05-14 21:21:43 -0300

    Adição de funções no nome do arquivo

--- TCP_Cliente.py
+++ TCP_Cliente.py
@@ -16,18 +16,128 @@ def titulo():
     print("--------------------\n")
 
 
-def print_envio(mensagem):
+def opcoes():
+    print('1) Solicitar arquivo.')
+    print('2) Chat.')
+    print('3) Conexão com o Servidor.\n')
+    
+
+def mensagem_envio(cliente_socket : s.socket, mensagem : str):
+    cliente_socket.send(mensagem.encode())
     print("--------------------")
     print('Enviado: ', mensagem)
     print("--------------------\n")
 
 
-def print_recebimento(mensagem_modificada, endereco_server):
+def mensagem_recebimento(cliente_socket : s.socket):
+    mensagem = cliente_socket.recv(TAM_BUFFER).decode('utf-8')
     print("--------------------")
-    print('Recebido: ', mensagem_modificada, ' - Servidor: ', endereco_server)
+    print('Recebido: ', mensagem)
     print("--------------------\n")
+    return mensagem
 
-# def main():
 
-cliente_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-cliente_socket.settimeout(60)
+def conectar_servidor():
+    inicializar = ''
+    iniciar_conexao = False
+    while inicializar == '':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        titulo()
+        inicializar = input("Deseja conectar com o servidor [S/N] ? ").lower()
+        match inicializar:
+            case 's':
+                iniciar_conexao = True
+            case 'sim':
+                iniciar_conexao = True
+            case 'n':
+                iniciar_conexao = False
+            case 'não':
+                iniciar_conexao = False
+            case _:
+                print('A escolha precisa estar nas opções acima!')
+                t.sleep(2)
+                inicializar = ''
+    return iniciar_conexao
+                
+
+def escolher_arquivo(cliente_socket : s.socket):
+    conjunto_arquivos = []
+    num_arquivos = cliente_socket.recv(TAM_BUFFER).decode('utf-8')
+    
+    if int(num_arquivos) < 0 or not isinstance(num_arquivos, int):
+        mensagem_envio(cliente_socket, 'ERROR-1-Má requisição')
+    else:
+        mensagem_envio(cliente_socket, 'OK-1-Confirmação')
+
+    for i in range(0, int(num_arquivos)):
+        recv_arquivo = mensagem_recebimento(cliente_socket)
+        conjunto_arquivos.append(recv_arquivo)
+        
+    arquivo_disponivel = False
+    while not arquivo_disponivel:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        titulo()
+        print('Arquivos disponíveis no servidor:')
+        for arquivo in conjunto_arquivos:
+            print(arquivo)
+
+        nome_arquivo = input("\nDigite o nome do arquivo que você deseja receber: ")
+        
+        mensagem_envio(cliente_socket, nome_arquivo)
+        
+        ok_arq = mensagem_recebimento(cliente_socket)
+        ok_arq = ok_arq.split("-")
+    
+        if(ok_arq[0] == 'OK'):
+            arquivo_disponivel = True
+        else:
+            print('A escolha precisa estar nas opções acima!')
+            t.sleep(2)
+
+
+def receber_arquivo(cliente_socket : s.socket):
+    print('Recebendo')
+    
+
+def requisitar_arquivo(cliente_socket : s.socket):
+    escolher_arquivo(cliente_socket)
+    receber_arquivo(cliente_socket)
+    
+
+def chat_servidor(cliente_socket : s.socket):
+    print('Olá')
+
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    cliente_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    cliente_socket.settimeout(30)
+
+    iniciar_conexao = conectar_servidor()
+    cliente_socket.connect(ENDERECO_IP)
+
+    opcao = 0
+    while iniciar_conexao:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        titulo()
+        opcoes()
+        opcao = int(input("Escolha uma opção: "))
+        
+        match opcao:
+            case 1:
+                requisitar_arquivo(cliente_socket)
+            case 2:
+                chat_servidor(cliente_socket)
+            case 3:
+                iniciar_conexao = conectar_servidor()
+            case _:
+                print('A escolha precisa estar nas opções acima!')
+                t.sleep(2)
+                print(opcao)
+                print(type(opcao))
+                opcao = 0
+                
+    cliente_socket.close()
+
+if __name__ == "__main__":
+    main()
--- TCP_Servidor.py
+++ TCP_Servidor.py
@@ -3,11 +3,12 @@ import time as t
 import logging as l
 import threading as th
 import hashlib as h
+import pathlib as p
 import os
 
 NOME_DO_SERVER = ''
-PORTA_DO_SERVER = 6000
 TAM_BUFFER = 2048
+PORTA_DO_SERVER = 6000
 ENDERECO_IP = (NOME_DO_SERVER, PORTA_DO_SERVER)
 
 # Funções padrão --------------------------------------------
@@ -18,38 +19,115 @@ def titulo():
     print("--------------------\n")
 
 
-def print_mensagem(mensagem, mensagem_modificada, endereco_server):
-    titulo()
+def mensagem_envio(servidor_socket : s.socket, mensagem : str):
+    servidor_socket.send(mensagem.encode())
     print("--------------------")
-    print('Recebido: ', mensagem, ' - Enviou: ', mensagem_modificada, ' - Cliente: ', endereco_server)
+    print('Enviado: ', mensagem)
     print("--------------------\n")
 
 
-# def main():
+def mensagem_recebimento(servidor_socket : s.socket):
+    mensagem = servidor_socket.recv(TAM_BUFFER).decode('utf-8')
+    print("--------------------")
+    print('Recebido: ', mensagem)
+    print("--------------------\n")
+    return mensagem
+    
 
-logger = l.getLogger(__name__)
-l.basicConfig(filename="server.log", encoding="utf-8", level=l.INFO, format="%(levelname)s - %(asctime)s: %(message)s")
+def iniciar_servidor():
+    inicializar = ''
+    iniciar_server = False
+    while inicializar == '':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        titulo()
+        inicializar = input("Deseja inicializar o servidor [S/N] ? ").lower()
+        match inicializar:
+            case 's':
+                iniciar_server = True
+            case 'sim':
+                iniciar_server = True
+            case 'n':
+                iniciar_server = False
+            case 'não':
+                iniciar_server = False
+            case _:
+                print('A escolha precisa estar nas opções acima!')
+                t.sleep(2)
+                inicializar = ''
+    return iniciar_server
 
-inicializar = ''
-iniciar_server = True
-while inicializar == '':
+
+def retornar_nome_arquivos(conexao_socket:s.socket):
     os.system('cls' if os.name == 'nt' else 'clear')
-    titulo()
-    inicializar = input("Deseja inicializar o servidor [S/N] ? ").lower()
-    match inicializar:
-        case 's':
-            iniciar_server = True
-        case 'sim':
-            iniciar_server = True
-        case 'n':
-            iniciar_server = False
-        case 'não':
-            iniciar_server = False
-        case _:
-            print('A escolha precisa estar nas opções acima!')
-            t.sleep(2)
-            erro_arquivo = ''
 
-os.system('cls' if os.name == 'nt' else 'clear')
-server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-server_socket.bind(ENDERECO_IP)
+    caminho = str(p.Path.cwd()) + '\Arquivos'
+    file_paths = os.listdir(caminho)
+    num_arquivos = len(file_paths)
+
+    conexao_socket.send(str(num_arquivos).encode())
+    
+    confirmacao_tam = conexao_socket.recv(TAM_BUFFER)
+    
+    if(confirmacao_tam != b'Recebido'):
+        conexao_socket.send("ERROR-1-Tamanho não recebido".encode())
+        return
+    
+    for i in range(0, num_arquivos):
+        conexao_socket.send(file_paths[i].encode())
+    
+    nome_arquivo = conexao_socket.recv(TAM_BUFFER).decode('utf-8')
+    
+    if not os.path.exists(os.path.join("./Arquivos", nome_arquivo)):
+        conexao_socket.send("ERROR-1-Arquivo não encontrado!".encode())
+        return
+    
+    
+def enviar_arquivo(conexao_socket:s.socket):
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    caminho = str(p.Path.cwd()) + '\Arquivos'
+    file_paths = os.listdir(caminho)
+    num_arquivos = len(file_paths)
+
+    conexao_socket.send(str(num_arquivos).encode())
+    ok_tam = conexao_socket.recv(TAM_BUFFER).decode('utf-8')
+    ok_tam = ok_tam.split("-")
+    
+    if(ok_tam[0] != 'OK'):
+        conexao_socket.send("ERROR-1-Tamanho não recebido".encode())
+        return
+    else:
+        for i in range(0, num_arquivos):
+            conexao_socket.send(file_paths[i].encode())
+        
+        nome_arquivo = conexao_socket.recv(TAM_BUFFER).decode('utf-8')
+        
+        if not os.path.exists(os.path.join("./Arquivos", nome_arquivo)):
+            conexao_socket.send("ERROR-1-Arquivo não encontrado!".encode())
+            return
+
+
+def main():
+    logger = l.getLogger(__name__)
+    l.basicConfig(filename="server.log", encoding="utf-8", level=l.INFO, format="%(levelname)s - %(asctime)s: %(message)s")
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    server_socket.bind(ENDERECO_IP)
+    server_socket.listen()
+    server_socket.settimeout(30)
+
+    iniciar_server = iniciar_servidor()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Esperando resposta')
+
+    while iniciar_server:
+        conexao_socket, endereco = server_socket.accept()
+        retornar_nome_arquivos(conexao_socket)
+        #thread = th.Thread(target=retornar_nome_arquivos, args=(conexao_socket), daemon=True)
+        #thread.start()
+        conexao_socket.close()
+        
+
+if __name__ == "__main__":
+    main()
